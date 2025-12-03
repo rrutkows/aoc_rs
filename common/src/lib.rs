256d2c118708da172a6c7bbeae4cba4f34f9c986
Rafał Rutkowski <rrutkowski@gmail.com>
2025-12-03 12:02:55 +0100

    Don't panic on get_input

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -2,7 +2,7 @@ use std::{fmt::Display, path::Path, time::Instant};
 
 pub fn get_input(package_dir: impl AsRef<Path>, day: u8) -> String {
     let path = package_dir.as_ref().join(format!("src/d{day:02}.txt"));
-    std::fs::read_to_string(path).expect("input txt should be present in src/dnn/")
+    std::fs::read_to_string(path).unwrap_or_default()
 }
 
 pub fn run<T, F>(f: F)
